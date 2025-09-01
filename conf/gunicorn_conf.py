b05d3f4a9f3e95d8e680849be0f0c0bfe557fbb0
HusseinSalem17 <husseinsalem177@gmail.com>
2025-09-01 22:48:48 +0300

    added render file

--- conf/gunicorn_conf.py
+++ conf/gunicorn_conf.py
@@ -1,4 +1,4 @@
-command = '/media/hussein/H.salem/fci_2020/computer_programming/Graduation_Project/Forme_API/venv/bin/gunicorn'
-pythonpath = '/media/hussein/H.salem/fci_2020/computer_programming/Graduation_Project/Forme_API'
-bind = '192.168.1.3:8000'
-workers = 3
+# command = '/media/hussein/H.salem/fci_2020/computer_programming/Graduation_Project/Forme_API/venv/bin/gunicorn'
+# pythonpath = '/media/hussein/H.salem/fci_2020/computer_programming/Graduation_Project/Forme_API'
+# bind = '192.168.1.3:8000'
+# workers = 3
